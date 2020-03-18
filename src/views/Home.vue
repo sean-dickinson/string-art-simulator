9cf7424e4412fd7dbb9fd80b6aca4488ebb19de4
Sean Dickinson <sedickinson9123@gmail.com>
2020-03-18 16:04:31 -0400

    (feat) added instruction for changing starting pt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <section class="section">
       <h1 class="title has-text-left">String Art Simulator</h1>
+      <p class="has-text-left">Click a hole to change the starting position of the string art</p>
       <canvas-container />
     </section>
   </div>
